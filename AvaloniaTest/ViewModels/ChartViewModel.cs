56d00b1694be6c7214ee489b28462bb2b3b9ab55
maks3211 <maks02inf@gmail.com>
2024-05-02 11:45:13 +0200

    prosty wykrtes

--- AvaloniaTest/ViewModels/ChartViewModel.cs
+++ AvaloniaTest/ViewModels/ChartViewModel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using LiveChartsCore;
+using LiveChartsCore.Kernel;
 using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore.SkiaSharpView.Painting;
 using LiveChartsCore.SkiaSharpView.VisualElements;
@@ -13,14 +14,31 @@ using SkiaSharp;
 
 namespace AvaloniaTest.ViewModels
 {
+
+    public record WeatherForecast(DateOnly date, int temp)
+    {
+        public static IEnumerable<WeatherForecast> TestData() =>
+            [
+            new WeatherForecast(new DateOnly(2024,4,21),14 ),
+             new WeatherForecast(new DateOnly(2024,4,22),7 ),
+             new WeatherForecast(new DateOnly(2024,4,23),8 ),
+             new WeatherForecast(new DateOnly(2024,4,24),10 ),
+            ];
+    }
+
+
+    
+
+
     public partial class ChartViewModel : ViewModelBase
     {
 
         public ISeries[] Series { get; set; } =
     {
-        new LineSeries<double>
+        new LineSeries<WeatherForecast>
         {
-            Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
+            Values = WeatherForecast.TestData(),
+            Mapping = (sample,_) => new Coordinate(sample.date.Day, sample.temp),
             Fill = null
         }
     };
